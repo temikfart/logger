2da51f65f37f8ea8a1c090ec72e41d58f158b2b0
Artyom Fartygin <fartygin.a@phystech.edu>
2022-09-28 12:52:07 +0300

    wip

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,23 +3,32 @@
 #include <algorithm>
 #include <string>
 
+namespace fs = std::filesystem;
+
 namespace utils {
+
+struct Time {
+    Time() : timestamp(std::chrono::system_clock::now()) {}
+
+    const std::chrono::time_point<std::chrono::system_clock> timestamp;
+};
 
 std::string to_upper(std::string str) {
     std::transform(str.begin(), str.end(), str.begin(), toupper);
     return str;
 }
-
-struct Time {
-    Time() : timestamp(std::chrono::system_clock::now()) {}
-    const std::chrono::time_point<std::chrono::system_clock> timestamp;
-};
-
 std::string to_string(const Time& time) {
     auto itt = std::chrono::system_clock::to_time_t(time.timestamp);
     std::ostringstream ss;
     ss << std::put_time(gmtime(&itt), "%F %T");
     return ss.str();
+}
+std::string to_filename(const Time& time, const fs::path& dir) {
+    auto filename = to_string(time);
+    std::replace_if(filename.begin(), filename.end(),
+                    [](auto& c) { return (c == ' ' || c == ':'); }, '-');
+    filename = dir.string() + '/' + filename + ".log";
+    return filename;
 }
 
 class NonCopyable {
@@ -48,5 +57,51 @@ private:
 
 template<class T>
 T* Singleton<T>::instance_ = nullptr;
+
+// TODO: remove it.
+void demo_perms(fs::perms p)
+{
+    std::cout << ((p & fs::perms::owner_read) != fs::perms::none ? "r" : "-")
+              << ((p & fs::perms::owner_write) != fs::perms::none ? "w" : "-")
+              << ((p & fs::perms::owner_exec) != fs::perms::none ? "x" : "-")
+              << ((p & fs::perms::group_read) != fs::perms::none ? "r" : "-")
+              << ((p & fs::perms::group_write) != fs::perms::none ? "w" : "-")
+              << ((p & fs::perms::group_exec) != fs::perms::none ? "x" : "-")
+              << ((p & fs::perms::others_read) != fs::perms::none ? "r" : "-")
+              << ((p & fs::perms::others_write) != fs::perms::none ? "w" : "-")
+              << ((p & fs::perms::others_exec) != fs::perms::none ? "x" : "-")
+              << std::endl;
+}
+
+class OutFileStream : public NonCopyable {
+public:
+    OutFileStream() = default;
+    OutFileStream(const std::filesystem::path& path) { this->set_path(path); }
+    ~OutFileStream() { if (this->is_open()) this->close(); }
+
+    void set_path(const std::filesystem::path& path) {
+        if (!fs::exists(path) || fs::is_regular_file(path))
+            path_ = path;
+    }
+    const std::filesystem::path& path() const { return path_; }
+
+    bool open(std::ios_base::openmode mode = std::ios::out) { output_.open(path_, mode); }
+    bool is_open() const { return output_.is_open(); }
+    void close() { if (this->is_open()) output_.close(); }
+
+    OutFileStream& operator<<(std::ostream& (*os)(std::ostream&)) {
+        output_ << os;
+        return (*this);
+    }
+    template <typename T>
+    OutFileStream& operator<<(const T& data) {
+        output_ << data;
+        return (*this);
+    }
+
+private:
+    std::filesystem::path path_;
+    std::ofstream output_;
+};
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "macro.hpp"
 
 int main() {
-    logger::Logger::init(logger::Severity::info, "../log/log.txt");
+    logger::Logger::init(logger::Severity::info, "../logs");
 
     LOG(logger::info) << "Hello, world!";
     LOGI << "Short form";
