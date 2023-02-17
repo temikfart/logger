f7fc0403281c644bd588a66d1a39dd6d88b1cd5d
Artyom Fartygin <fartygin.a@phystech.edu>
2023-02-17 14:42:37 +0300

    restore main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,10 @@
 #include "log.hpp"
 
 int main() {
-    logger::init<logger::JSONFormatter>(logger::Severity::info, "../log");
-    logger::init<logger::JSONFormatter>(logger::Severity::debug, logger::cout);
+    logger::init(logger::Severity::info, "../log");
 
     LOG(logger::info) << "Hello, world!";
     LOGI << "Short form";
-    LOGF << "FATAL ERROR";
-
-    logger::Logger::get()->turn_colours_off();
-
-    LOGE << "ERROR";
-
-    logger::Logger::get()->turn_colours_on();
-
-    LOGW << "WARNING";
-    LOGT << "TRACE";
-    LOGD << "DEBUG";
-
-    logger::Logger::get()->change_colours(logger::Severity::warning,
-                                      {logger::Colour::red, logger::Colour::common});
-
-    LOGW << "WARNING";
 
     return 0;
 }
