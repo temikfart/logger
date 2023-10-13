d4701f0750f23d501b03b59941618e3fda73c0a0
Artyom Fartygin <78689166+temikfart@users.noreply.github.com>
2023-10-13 16:03:51 +0300

    [#15] Remove warnings during compilation (#16)
    Fixes #15.
    
    - Added new compilation flags `-Wextra -Wall -Werror` to force fix all
    warnings;
    - Fixed existed warnings.

--- include/logger/appenders/appender_interface.hpp
+++ include/logger/appenders/appender_interface.hpp
@@ -16,12 +16,13 @@ enum AppenderType {
 class IAppender {
 public:
     IAppender(AppenderType type) : appender_type_(type) {}
-    IAppender(AppenderType type, Severity severity) : appender_type_(type), severity_(severity) {}
+    IAppender(AppenderType type, Severity severity) : severity_(severity), appender_type_(type) {}
+    virtual ~IAppender() = default;
     virtual void write(const Record& record) = 0;
     Severity severity() const { return severity_; }
     void set_severity(Severity severity) { severity_ = severity; }
     AppenderType type() const { return appender_type_; }
-    virtual void set_colours(Severity severity, const MessageColours& msg_cols) {}
+    virtual void set_colours(Severity /*unused*/, const MessageColours& /*unused*/) {}
     virtual void turn_colours_on() {}
     virtual void turn_colours_off() {}
 //    virtual ~IAppender(); // TODO: See Vladimirov's lectures
